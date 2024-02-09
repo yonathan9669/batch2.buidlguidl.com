86741e00e8092a3fd2f8223c6c1b8f251a8fe7e8
👻 <y@uklok.ai>
2024-02-09 16:09:29 -0400

    refactor(api): 👻 Static Content
    - The API endpoints were refactored to load the data from a static JSON.
    - All external calls were removed.
    
    This ref #8

--- packages/nextjs/pages/api/uklok/links.ts
+++ packages/nextjs/pages/api/uklok/links.ts
@@ -1,19 +1,12 @@
+import { links as LINKS } from "./data.json";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-const UKLOK_API_URL = "https://uklok.hasura.app/api/rest";
 type APILink = { url: string; platform: { name: string; description: string } };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") throw new Error("Invalid method");
 
-  const config = {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  };
-  const linksRes = await fetch(`${UKLOK_API_URL}/v0/third_party/links`, config);
-  const { links: array } = await linksRes.json();
-
-  const links = (array as Array<APILink>).map(({ url, platform: { name, description } }) => ({
+  const links = (LINKS as Array<APILink>).map(({ url, platform: { name, description } }) => ({
     name,
     description,
     url,
